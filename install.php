b6407922c1ab4d5891fad5609c85b8d5098c9275
yixue <v7e0b@ugrad.cs.ubc.ca>
2018-11-16 02:31:24 -0800

    frontend UI in progress

--- install.php
+++ install.php
@@ -1,19 +1,42 @@
 <?php
 
-/**
- * Open a connection via PDO to create a
- * new database and table with structure.
- *
- */
+// Name of the file
+$filename = 'fakesteam.sql';
+// MySQL host
+$mysql_host = 'localhost';
+// MySQL username
+$mysql_username = 'root';
+// MySQL password
+$mysql_password = 'root';
+// Database name
+$mysql_database = 'fakesteam';
 
-require "config.php";
+// Connect to MySQL server
+mysql_connect($mysql_host, $mysql_username, $mysql_password) or die('Error connecting to MySQL server: ' . mysql_error());
+// Select database
+mysql_select_db($mysql_database) or die('Error selecting MySQL database: ' . mysql_error());
 
-try {
-    $connection = new PDO("mysql:host=localhose", $username, $password, $options);
-    $sql = file_get_contents("data/init.sql");
-    $connection->exec($sql);
+// Temporary variable, used to store current query
+$templine = '';
+// Read in entire file
+$lines = file($filename);
+// Loop through each line
+foreach ($lines as $line)
+{
+// Skip it if it's a comment
+    if (substr($line, 0, 2) == '--' || $line == '')
+        continue;
 
-    echo "Database and table users created successfully.";
-} catch(PDOException $error) {
-    echo $sql . "<br>" . $error->getMessage();
+// Add this line to the current segment
+    $templine .= $line;
+// If it has a semicolon at the end, it's the end of the query
+    if (substr(trim($line), -1, 1) == ';')
+    {
+        // Perform the query
+        mysql_query($templine) or print('Error performing query \'<strong>' . $templine . '\': ' . mysql_error() . '<br /><br />');
+        // Reset temp variable to empty
+        $templine = '';
+    }
 }
+echo "Tables imported successfully";
+?>
