e73959b308582781585137b83d0addd8db5c035e
Niloofar Gharavi <niloofar_nemone@yahoo.com>
2018-11-16 19:18:04 -0800

    php files

---
+++ member.php
@@ -0,0 +1,371 @@
+<!--Project File for UBC CPSC 304
+  IF YOU HAVE A TABLE CALLED "tab1" IT WILL BE DESTROYED
+
+  The script assumes you already have a server set up
+  All OCI commands are commands to the Oracle libraries
+  To get the file to work, you must place it somewhere where your
+  Apache server can run it, and you must rename it to have a ".php"
+  extension.  You must also change the username and password on the 
+  OCILogon below to be your ORACLE username and password -->
+
+
+
+<p>If you wish to reset the table, press the reset button. If this is the first time you're running this page, you MUST use reset</p>
+<form method="POST" action="member.php">
+   
+<p><input type="submit" value="Reset" name="reset"></p>
+<p><input type="submit" value="run hardcoded queries" name="dostuff"></p>
+</form>
+
+<p>Add another member as friend:</p>
+
+<form method="POST" action="member.php">
+<p><table>
+  <tr>
+    <td><font size="2">My Member ID:</font></td>
+    <td><font size="2">My friend's Member ID:</font></td>
+    </tr>
+  <tr>
+    <td><input type="text" name="MyMID" size="20"></td>
+    <td><input type="text" name="FriendMID" size="20"></td>
+  </tr>
+  <tr>
+    <td><input type="submit" value="Add Friend" name="AddFriend"></p></td>
+  </tr>
+</table>
+</form>
+
+<p> Update My Username and Password: </p>
+<form method="POST" action="member.php">
+
+    <p><table>
+        <tr>
+            <td><font size="2">My ID</font></td>
+        </tr>
+        <tr>
+            <td><input type="text" name="MyID" size="30"></td>
+        </tr>
+        <tr>
+            <td><font size="2">New Username</font></td>
+            <td><font size="2">New Password</font></td>
+        </tr>
+        <tr>
+            <td><input type="text" name="updateUsername" size="30"></td>
+            <td><input type="text" name="updatePassword" size="30"></td>
+        </tr>
+        <tr>
+            <td><input type="submit" value="Update My Information" name="updateMyInformation"></p></td>
+    </tr>
+    </table></p>
+</form>
+
+<p> Add a game to wishlist by ID: </p>
+<form method="POST" action="member.php">
+<p><table>
+  <tr>
+    <td><font size="2">Game ID</font></td>
+  </tr>
+  <tr>
+    <td><input type="text" name="toWishlistGID" size="12"></td>
+  </tr>
+  <tr>
+    <td><font size="2">Wishlist ID</font></td>
+    </tr>
+  <tr>
+    <td><input type="text" name="addGameToWishlist" size="20"></td>
+  </tr>
+  <tr>
+    <td><input type="submit" value="Add Game To Wishlist" name="addGameToWishlist"></p></td>
+  </tr>
+</table>
+
+</form>
+
+<p> Search For Currently On Sale Games In My Wishlist: </p>
+<form method="POST" action="member.php">
+<!--refresh page when submit-->
+
+<p><table>
+  <tr>
+    <td><font size="2">My Wishlist ID</font></td>
+  </tr>
+  <tr>
+    <td><input type="text" name="WID" size="12"></td>
+  </tr>
+  <tr>
+    <td><input type="submit" value="Search Games" name="searchOnSaleGamesByWID"></p></td>
+  </tr>
+</table></p>
+</form>
+
+<p> Add a game to the OnSaleList: </p>
+<form method="POST" action="developer.php">
+
+<p><table>
+  <tr>
+    <td><font size="2">Game ID</font></td>
+  </tr>
+  <tr>
+    <td><input type="text" name="addSaleGameID" size="12"></td>
+  </tr>
+  <tr>
+    <td><input type="submit" value="Start Sale" name="addSaleSubmit"></p></td>
+  </tr>
+</table></p>
+</form>
+
+<p> Update existing games in the OnSaleList: </p>
+<form method="POST" action="developer.php">
+
+<p><table>
+  <tr>
+    <td><font size="2">Game ID</font></td>
+  </tr>
+  <tr>
+    <td><input type="text" name="updateSaleGameID" size="12"></td>
+  </tr>
+  <tr>
+    <td><input type="submit" value="Update Sale" name="updateSaleSubmit"></p></td>
+  </tr>
+</table></p>
+</form>
+
+<p> Delete a game from the OnSaleList: </p>
+<form method="POST" action="developer.php">
+<!--refresh page when submit-->
+
+<p><table>
+  <tr>
+    <td><font size="2">Game ID</font></td>
+  </tr>
+  <tr>
+    <td><input type="text" name="deleteSaleGameID" size="12"></td>
+  </tr>
+  <tr>
+    <td><input type="submit" value="End Sale" name="deleteSaleSubmit"></p></td>
+  </tr>
+</table></p>
+</form>
+
+
+<a href="steam.php">Back to home</a>
+
+<?php
+
+//this tells the system that it's no longer just parsing 
+//html; it's now parsing PHP
+
+$success = True; //keep track of errors so it redirects the page only if there are no errors
+$db_conn = OCILogon("ora_n9y0b", "a57734162", "dbhost.ugrad.cs.ubc.ca:1522/ug");
+
+function executePlainSQL($cmdstr) { //takes a plain (no bound variables) SQL command and executes it
+	//echo "<br>running ".$cmdstr."<br>";
+	global $db_conn, $success;
+	$statement = OCIParse($db_conn, $cmdstr); //There is a set of comments at the end of the file that describe some of the OCI specific functions and how they work
+
+	if (!$statement) {
+		echo "<br>Cannot parse the following command: " . $cmdstr . "<br>";
+		$e = OCI_Error($db_conn); // For OCIParse errors pass the       
+		// connection handle
+		echo htmlentities($e['message']);
+		$success = False;
+	}
+
+	$r = OCIExecute($statement, OCI_DEFAULT);
+	if (!$r) {
+		echo "<br>Cannot execute the following command: " . $cmdstr . "<br>";
+		$e = oci_error($statement); // For OCIExecute errors pass the statementhandle
+		echo htmlentities($e['message']);
+		$success = False;
+	} else {
+
+	}
+	return $statement;
+
+}
+
+function executeBoundSQL($cmdstr, $list) {
+	/* Sometimes the same statement will be executed for several times ... only
+	 the value of variables need to be changed.
+	 In this case, you don't need to create the statement several times; 
+	 using bind variables can make the statement be shared and just parsed once.
+	 This is also very useful in protecting against SQL injection.  
+      See the sample code below for how this functions is used */
+
+	global $db_conn, $success;
+	$statement = OCIParse($db_conn, $cmdstr);
+
+	if (!$statement) {
+		echo "<br>Cannot parse the following command: " . $cmdstr . "<br>";
+		$e = OCI_Error($db_conn);
+		echo htmlentities($e['message']);
+		$success = False;
+	}
+
+	foreach ($list as $tuple) {
+		foreach ($tuple as $bind => $val) {
+			//echo $val;
+			//echo "<br>".$bind."<br>";
+			OCIBindByName($statement, $bind, $val);
+			unset ($val); //make sure you do not remove this. Otherwise $val will remain in an array object wrapper which will not be recognized by Oracle as a proper datatype
+
+		}
+		$r = OCIExecute($statement, OCI_DEFAULT);
+		if (!$r) {
+			echo "<br>Cannot execute the following command: " . $cmdstr . "<br>";
+			$e = OCI_Error($statement); // For OCIExecute errors pass the statement handle
+			echo htmlentities($e['message']);
+			echo "<br>";
+			$success = False;
+		}
+	}
+
+}
+
+function printResult($result) { //prints results from a select statement
+	echo "<br>Got data from table tab1:<br>";
+	echo "<table>";
+	echo "<tr><th>ID</th><th>Name</th><th>Gender</th></tr>";
+
+	while ($row = OCI_Fetch_Array($result, OCI_BOTH)) {
+		echo "<tr><td>" . $row[0] . "</td><td>" . $row[1] . "</td><td>" . $row[2] . "</td></tr>"; //or just use "echo $row[0]"
+	}
+	echo "</table>";
+
+}
+
+// Connect Oracle...
+if ($db_conn) {
+
+	if (array_key_exists('reset', $_POST)) {
+		// Drop old table...
+		echo "<br> dropping table <br>";
+		executePlainSQL("Drop table tab1");
+
+		// Create new table...
+		echo "<br> creating new table <br>";
+		executePlainSQL("create table tab1 (nid number, name varchar2(30), gender varchar2(20), primary key (nid))");
+		OCICommit($db_conn);
+
+	} else
+		if (array_key_exists('insertsubmit', $_POST)) {
+			//Getting the values from user and insert data into the table
+			if(isset($_POST['gameGenre']) ) {
+				$varGenre = $_POST['gameGenre'];
+			}
+			$tuple = array (
+				":bind1" => $_POST['insNo'],
+				":bind2" => $_POST['insName'],
+        		":bind4" => $_POST['insGender']
+			);
+			$alltuples = array (
+				$tuple
+			);
+			executeBoundSQL("insert into tab1 values (:bind1, :bind2, :bind3)", $alltuples);
+			OCICommit($db_conn);
+
+		} else
+			if (array_key_exists('updatesubmit', $_POST)) {
+				// Update tuple using data from user
+				$tuple = array (
+					":bind1" => $_POST['oldName'],
+					":bind2" => $_POST['newName'],
+          ":bind3" => $_POST['gender']
+				);
+				$alltuples = array (
+					$tuple
+				);
+				executeBoundSQL("update tab1 set name=:bind2 where name=:bind1 and gender=:bind3", $alltuples);
+				OCICommit($db_conn);
+
+			} else
+      if (array_key_exists('deletesubmit', $_POST)) {
+        // Update tuple using data from user
+        $tuple = array (
+          ":bind2" => $_POST['deleteName'],
+          ":bind3" => $_POST['deleteGender']
+        );
+        $alltuples = array (
+          $tuple
+        );
+        executeBoundSQL("delete from tab1 where name=:bind2 and gender=:bind3", $alltuples);
+        OCICommit($db_conn);
+
+      } else
+				if (array_key_exists('dostuff', $_POST)) {
+					// Insert data into table...
+					executePlainSQL("insert into tab1 values (10, 'Frank', 'Male')");
+					// Inserting data into table using bound variables
+					$list1 = array (
+						":bind1" => 6,
+						":bind2" => "All",
+            ":bind3" => "Male"
+					);
+					$list2 = array (
+						":bind1" => 7,
+						":bind2" => "John",
+            ":bind3" => "Male"
+					);
+					$allrows = array (
+						$list1,
+						$list2
+					);
+					executeBoundSQL("insert into tab1 values (:bind1, :bind2, :bind3)", $allrows); //the function takes a list of lists
+					// Update data...
+					//executePlainSQL("update tab1 set nid=10 where nid=2");
+					// Delete data...
+					//executePlainSQL("delete from tab1 where nid=1");
+					OCICommit($db_conn);
+				}
+
+	if ($_POST && $success) {
+		//POST-REDIRECT-GET -- See http://en.wikipedia.org/wiki/Post/Redirect/Get
+		header("location: developer.php");
+	} else {
+		// Select data...
+		$result = executePlainSQL("select * from tab1");
+		printResult($result);
+	}
+
+	//Commit to save changes...
+	OCILogoff($db_conn);
+} else {
+	echo "cannot connect";
+	$e = OCI_Error(); // For OCILogon errors pass no handle
+	echo htmlentities($e['message']);
+}
+
+
+/* OCILogon() allows you to log onto the Oracle database
+     The three arguments are the username, password, and database.
+     You will need to replace "username" and "password" for this to
+     to work. 
+     all strings that start with "$" are variables; they are created
+     implicitly by appearing on the left hand side of an assignment 
+     statement */
+/* OCIParse() Prepares Oracle statement for execution
+      The two arguments are the connection and SQL query. */
+/* OCIExecute() executes a previously parsed statement
+      The two arguments are the statement which is a valid OCI
+      statement identifier, and the mode. 
+      default mode is OCI_COMMIT_ON_SUCCESS. Statement is
+      automatically committed after OCIExecute() call when using this
+      mode.
+      Here we use OCI_DEFAULT. Statement is not committed
+      automatically when using this mode. */
+/* OCI_Fetch_Array() Returns the next row from the result data as an  
+     associative or numeric array, or both.
+     The two arguments are a valid OCI statement identifier, and an 
+     optinal second parameter which can be any combination of the 
+     following constants:
+
+     OCI_BOTH - return an array with both associative and numeric 
+     indices (the same as OCI_ASSOC + OCI_NUM). This is the default 
+     behavior.  
+     OCI_ASSOC - return an associative array (as OCI_Fetch_Assoc() 
+     works).  
+     OCI_NUM - return a numeric array, (as OCI_Fetch_Row() works).  
+     OCI_RETURN_NULLS - create empty elements for the NULL fields.  
+     OCI_RETURN_LOBS - return the value of a LOB of the descriptor.  
+     Default mode is OCI_BOTH.  */
+?>
---
+++ steam.php
@@ -0,0 +1,6 @@
+<p>CPSC 304 Game Library</p>
+
+<p>Choose User Type:</p>
+<input name="newThread" type="button" value="Game Developer" onclick="window.open('developer.php')"/>
+<input name="newThread" type="button" value="Anonymous Customer" onclick="window.open('customer.php')"/>
+<input name="newThread" type="button" value="Registered Member" onclick="window.open('member.php')"/>
