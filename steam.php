a8f89d7640affa4e2c7106b872bb0efb3e0ce6ff
unknown <David>
2018-11-18 16:14:52 -0800

    finalized

--- steam.php
+++ steam.php
@@ -1,6 +1,22 @@
-<p>CPSC 304 Game Library</p>
+<!--Project File for UBC CPSC 304
+  David (Yu Feng) Guo, Yixue Xu, Brandon Yip, Niloofar Gharavi -->
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <link href="style.php" type="text/css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:400,700" rel="stylesheet">
+  </head>
+
+<div class="center">
+<p class="title">CPSC 304 GAME LIBRARY</p>
 
 <p>Choose User Type:</p>
 <input name="newThread" type="button" value="Game Developer" onclick="window.open('developer.php')"/>
 <input name="newThread" type="button" value="Anonymous Customer" onclick="window.open('customer.php')"/>
 <input name="newThread" type="button" value="Registered Member" onclick="window.open('member.php')"/>
+
+<p>Search Game Library:</p>
+<input name="newThread" type="button" value="Open Search Engine" onclick="window.open('search.php')"/>
+</div>
+</html>
